e7878b6b95f3bd27c26e299fe9f5715c540ce45c
Felipe Corrêa Telles <felipecorreatelles@gmail.com>
2020-02-25 20:25:21 -0300

    removendo rota de teste

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 
 import ProductPage from './pages/ProductPage';
 import CheckoutPage from './pages/CheckoutPage';
-import ReactForm from './components/react-form/ReactForm';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={ProductPage} />
       <Route path="/checkout" component={CheckoutPage} />
-      <Route path="/form" component={ReactForm} />
     </Switch>
   );
 }
